f196471f2b980073d233bc3ec534d55f2fd41b2d
david2777 <daduvo11@gmail.com>
2015-12-04 21:49:34 -0500

    Add new CMDTicket and error checks for JobTicket

--- JobTicket.py
+++ JobTicket.py
@@ -14,17 +14,39 @@ from MySQLSetup import hydra_job, hydra_jobboard, hydra_taskboard, hydra_rendert
 #Note:Probably needs overhaul
 
 class UberJobTicket:
-    """
-    A new Ticket Class for creating jobs and tasks
-    """
+    """A Ticket Class for submitting jobs and their subtasks."""
     def __init__(self, baseCMD, startFrame, endFrame, byFrame, taskFile, priority,
                 phase, jobStatus, niceName, owner, compatabilityPattern):
-        #Verify data input here! Raise errors before they hit the DB!
+        #Let's verify the data we just got from the user
+        if len(baseCMD) > 255:
+            raise Exception("baseCMD too long! baseCMD must be less than 255 characters!")
+        if startFrame > endFrame:
+            raise Exception("startFrame is greater than endFrame!")
+        if 0 < startFrame > 65535 or 0 < endFrame > 65535:
+            raise Exception("Frame range out of range! Start/End frames must be between 0 and 65535!")
+        if 0 < byFrame > 255:
+            raise Exception("byFrame out of range! byFrame must be between 0 and 255!")
+        if len(taskFile) > 255:
+            raise Exception("taskFile out of range! taskFile path must be less than 255 characters!")
+        if 0 < priority > 255:
+            raise Exception("Priority out of range! Priority must be between 0 and 255!") 
+        if 0 < phase > 255:
+            raise Exception("Phase out of range! Phase must be between 0 and 255!")
+        if len(jobStatus) > 1:
+            raise Exception("jobStatus out of range! jobStatus must only be one character!")
+        if len(niceName) > 60:
+            raise Exception("NiceName out of range! NiceName must be less than 60 characters!")
+        if len(owner) > 45:
+            raise Exception("Owner out of range! Owner must be less than 45 characters!")
+        if len(compatabilityPattern) > 255:
+            raise Exception("compatabilityPattern out of range! compatabilityPattern must be less than 255 characters!")
+        
+        #Looks good, let's setup our class variables
         self.baseCMD = baseCMD          #VARCHAR(255)
         self.startFrame = startFrame    #SMALLINT   ie.(0-65535)
         self.endFrame = endFrame        #SMALLINT   ie.(0-65535)
         self.byFrame = byFrame          #TINYINT   ie.(0-255)
-        self.taskFile = taskFile        #VARCHAR(60)
+        self.taskFile = taskFile        #VARCHAR(255)
         self.priority = priority        #TINYINT    ie.(0-255)
         self.phase = phase              #TINYINT    ie.(0-255)
         self.jobStatus = jobStatus      #CHAR(1)
@@ -36,9 +58,11 @@ class UberJobTicket:
     def commandBuilder(self, startFrame, endFrame):
         """Returns a command as a list for sending to subprocess.call on RenderNode"""
         #Using -mr:v 5 to get a more verbose log, render should still use correct engine
-        return [self.baseCMD, '-mr:v', '5', '-s', str(startFrame), '-e', str(endFrame), self.taskFile]
-        
+        return [self.baseCMD, '-mr:v', '5', '-s', str(startFrame), '-e', str(endFrame), self.taskFile]    
+    
     def createJob(self):
+        """Function for building and inserting a job.
+        Returns the job so we can use the ID for the tasks""" 
         job = hydra_jobboard(baseCMD = self.baseCMD,
                             startFrame = self.startFrame,
                             endFrame = self.endFrame,
@@ -58,6 +82,7 @@ class UberJobTicket:
         return job
         
     def createTasks(self):
+        """Function for creating and inserting tasks for a range of frames."""
         frameRange = range(self.startFrame, self.endFrame)
         frameList = frameRange[0::self.byFrame]
         if self.endFrame not in frameList:
@@ -77,11 +102,43 @@ class UberJobTicket:
                 task.insert(transaction=t)
                   
     def doSubmit(self):
+        """The actual function to submit the job and tasks to the DB"""
         job = self.createJob()
         self.job_id = job.job_id
         self.createTasks()
-        logger.info("Submitted!")
+        logger.info("Submitted UberTicket job with id %d" % self.job_id)
+        
+    def createCMDTicketTask(self):
+        """Command for making a task for a CMD Ticket.
+        
+        Not sure why you'd ever need a CMD Ticket but it was in the original
+        Hydra so I figured I'd include it in the new UberTicket
+        
+        ***UNTESTED***"""
+        command = [self.baseCMD]
+        logger.debug(command)
+        task = hydra_taskboard(command = repr(command),
+                              job_id = self.job_id,
+                              task_status = self.jobStatus,
+                              priority = self.priority,
+                              createTime = self.createTime,
+                              requirements = self.compatabilityPattern
+                              )
+        
+        with transaction() as t:
+            task.insert(transaction=t)
             
+    def doSubmitCMDTicket(self):
+        """Command for submitting a CMDTicket.
+        
+        Not sure why you'd ever need a CMD Ticket but it was in the original
+        Hydra so I figured I'd include it in the new UberTicket
+        
+        ***UNTESTED***"""
+        job = self.createJob()
+        self.job_id = job.job_id
+        self.createCMDTicketTask()
+        logger.info("Submitted UberTicket CMDTicket job with id %d" % self.job_id)
 
 class JobTicket:
     """
@@ -162,15 +219,6 @@ class MayaTicket(JobTicket):
             with transaction() as t:
                 task.insert(transaction=t)
 
-# these classes are selected by the submitter GUI
-class Maya2011 (MayaTicket):
-    executable = r'c:\program files\autodesk\maya2011\bin\render.exe'
-
-class Maya2013 (MayaTicket):
-    executable = r'c:\program files\autodesk\maya2013\bin\render.exe'
-
-class Maya2014 (MayaTicket):
-    executable = r'c:\program files\autodesk\maya2014\bin\render.exe'
 
 class CMDTicket(JobTicket):
     """A job ticket for shoehorning arbitrary commands into the task list. You 
@@ -191,6 +239,7 @@ class CMDTicket(JobTicket):
                                  createTime = job.createTime)
         with transaction() as t:
             task.insert(transaction=t)
+
             
 if __name__ == "__main__":
     prompt = raw_input("Create test Job? ").lower()
@@ -206,7 +255,7 @@ if __name__ == "__main__":
         compatabilityPattern = ""
         
         uberPhase01 = UberJobTicket(baseCMD, startFrame, endFrame, 10, mayaFile,
-        priority, "1", jobStatus, niceName + "_PHASE_01", owner, compatabilityPattern)
+        priority, 1, jobStatus, niceName + "_PHASE_01", owner, compatabilityPattern)
         uberPhase01.doSubmit()
         
     raw_input("DONE...")
