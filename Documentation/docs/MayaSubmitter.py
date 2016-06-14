039ff63a74bd91e8f1cacf360e56fe680dde8eba
david2777 <daduvo11@gmail.com>
2016-06-13 22:36:21 -0400

    Update Maya Submitter

--- Documentation/docs/MayaSubmitter.py
+++ Documentation/docs/MayaSubmitter.py
@@ -6,20 +6,24 @@ import subprocess
 import maya.cmds as cmds
 import maya.mel as mel
 
-#Get data from Maya
+#-----------------Get Data from Maya------------------#
 reqsList = []
 renderLayersList = []
 engineDict = {"redshift":"Redshift", "mentalRay":"MentalRay", "vray":"VRay", "renderMan":"RenderMan", "renderManRIS":"RenderMan", "arnold":"Arnold"}
 sceneFile = cmds.file(q = True, exn = True)
-project = cmds.workspace(q = True, rd = True)
+projectDir = cmds.workspace(q = True, rd = True)
 startFrame = cmds.getAttr("defaultRenderGlobals.startFrame")
 endFrame = cmds.getAttr("defaultRenderGlobals.endFrame")
-#Get renderable camera here
-cmd = ""
+camList = [x for x in cmds.ls(type="camera") if cmds.getAttr(x+".renderable")]
+try:
+    renderCam = camList[0]
+except IndexError:
+    renderCam = None
 
 currentEngine = mel.eval("currentRenderer")
 if currentEngine in engineDict.keys():
     reqsList.append(engineDict[currentEngine])
+reqsReturn  = ",".join(reqsList)
 
 renderLayers = cmds.listConnections("renderLayerManager")
 for layer in renderLayers:
@@ -27,15 +31,34 @@ for layer in renderLayers:
         renderLayersList.append(str(layer))
 renderLayersReturn = ",".join(renderLayersList)
 
-reqsReturn  = ",".join(reqsList)
+#----------Setup Base Submitter Command Line Args----------#
+command = "python C:\\Users\\David\\Documents\\GitHub\\DPS_Hydra\\SubmitterMain.py \"{0}\" -s \"{1}\" -e \"{2}\" -p \"{3}\"".format(sceneFile, startFrame, endFrame, projectDir)
 
-command = "python C:\\Users\\David\\Documents\\GitHub\\DPS_Hydra\\SubmitterMain.py {0} -s {1} -e {2} -p {3}".format(sceneFile, startFrame, endFrame, project)
-if cmd != "":
-    command += " -m {0}".format(cmd)
+#------------Build Extra Maya Command Line Args------------#
+extraCmdList = []
+#Confirm Camera Here
+if renderCam:
+    extraCmdList.append("-cam {0}".format(renderCam))
+
+#Build Render Directory here
+renderDir = "//This/Is/A/Test"
+if renderDir:
+    extraCmdList.append("-rd {0}".format(renderDir))
+
+if extraCmdList != []:
+    command += " -m \"{0}\"".format(" ".join(extraCmdList))
+
+#----------Setup Extra Submitter Command Line Args----------#
+#Build Project Name Here
+projectName = "TestProject"
+if projectName:
+    command += " -q \"{0}\"".format(projectName)
+
 if reqsReturn != "":
-    command += " -c {0}".format(reqsReturn)
+    command += " -c \"{0}\"".format(reqsReturn)
+
 if renderLayersReturn != "":
-    command += " -r {0}".format(renderLayersReturn)
+    command += " -r \"{0}\"".format(renderLayersReturn)
 
 print command
 subprocess.check_call(command)
