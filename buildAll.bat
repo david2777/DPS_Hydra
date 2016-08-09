ac85fc83461880dbce2f56a90f49857f02098a2c
david2777 <daduvo11@gmail.com>
2016-08-08 20:40:51 -0400

    Minor doc updates

--- buildAll.bat
+++ buildAll.bat
@@ -1,6 +1,6 @@
 pyinstaller -F FarmView.spec
-pyinstaller -F RenderNodeMain.spec
 pyinstaller -F RenderManagerServer.spec
+pyinstaller -F RenderNode.spec
 pyinstaller -F Register.spec
 pyinstaller -F SubmitterMain.spec
 pyinstaller -F FrameUpdater.spec
